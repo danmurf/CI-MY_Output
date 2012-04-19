df01570b29a7a3eb93d284726cdf5c1a767c768f
Dan Murfitt <dan@murfitt.net>
2012-04-19 22:10:59 +0100

    Added MY_Output.php template

---
+++ MY_Output.php
@@ -0,0 +1,38 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+ *     Copyright (C) 2012  Dan Murfitt
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Extended Output Class
+ *
+ * Adds additional functionality to the Output library.
+ *
+ * @package		CodeIgniter
+ * @subpackage          Libraries
+ * @category            Output
+ * @author		Dan Murfitt
+ * @link		http://murfitt.net
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License (GPLv3)
+ */
+class MY_Output extends CI_Output {
+    
+}
+// END MY Output Class
+
+/* End of file Output.php */
+/* Location: ./application/core/Output.php */
